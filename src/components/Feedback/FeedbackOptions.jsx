db8edaf9fbdd3cd4debb7e82bc1be35993c645b3
Ivan Kapustian <michiro2845@gmail.com>
2023-09-08 00:04:00 +0300

    done hw

--- src/components/Feedback/FeedbackOptions.jsx
+++ src/components/Feedback/FeedbackOptions.jsx
@@ -3,11 +3,17 @@ import React from 'react';
 const FeedbackOptions = ({ options }) => {
   return (
     <div>
+      {options.map(option => (
+        <button key={option.name} onClick={option.leaveFeedback}>
+          {option.name}
+        </button>
+      ))}
+      {/*                ALTERNATIVE
       {options.map((option, index) => (
         <button key={index} onClick={option.leaveFeedback}>
           {option.name}
         </button>
-      ))}
+      ))} */}
     </div>
   );
 };
